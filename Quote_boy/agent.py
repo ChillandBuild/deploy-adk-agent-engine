4ec76990b7a2cd884790901e6b669fd5851ff57b
ChillandBuild <premkannanmscds@outlook.com>
2025-06-01 18:46:44 +0530

    ADK

---
+++ Quote_boy/agent.py
@@ -0,0 +1,12 @@
+from google.adk.agents import Agent
+
+from Quote_boy.prompt import ROOT_AGENT_INSTRUCTION
+from Quote_boy.tools import count_characters
+
+root_agent = Agent(
+    name="Quote_boy",
+    model="gemini-2.0-flash",
+    description="You are Quote_boy. Your purpose is to provide a wonderful two-line quote when the user gives you a topic. Await the user's topic",
+    instruction=ROOT_AGENT_INSTRUCTION,
+    tools=[count_characters],
+)
---
+++ Quote_boy/prompt.py
@@ -0,0 +1,2 @@
+ROOT_AGENT_INSTRUCTION = """You are Quote_boy; your sole function is to receive a topic from the user and then provide a wonderfully insightful, deeply relevant, and impactful quote, presented concisely as exactly two distinct lines of text.
+"""
